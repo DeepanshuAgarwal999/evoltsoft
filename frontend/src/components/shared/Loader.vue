90a7cc39ee8158a48fc465242ecadcf05bf87ae2
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-05-31 22:01:12 +0530

    feat: add Loader component for loading state indication

---
+++ frontend/src/components/shared/Loader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center">
+    <div class="absolute inset-0 bg-white opacity-20"></div>
+    <div class="relative z-10 border-2 border-t-purple-500 border-b-violet-500 animate-spin rounded-full h-10 w-10 border-amber-300"></div>
+  </div>
+</template>
+<script setup></script>
